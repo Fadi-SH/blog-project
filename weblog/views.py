3ba25fcbf432ff96f2849927a6b88394dec7bf11
Fadi-SH <fadihammad914@gmail.com>
2019-03-10 12:01:28 -0400

    added pagination

--- weblog/views.py
+++ weblog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 
 from .models import Weblog
 
 
 def weblog(request):
-    weblogs = Weblog.objects.order_by('-pub_date')
+    weblogs_list = Weblog.objects.order_by('-pub_date')
+    paginator = Paginator(weblogs_list, 2)
+
+    page = request.GET.get('page')
+    weblogs = paginator.get_page(page)
+
     return render(request, 'weblog/weblog.html', {'weblogs': weblogs})
 
 
